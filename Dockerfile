a4a2f5886fa5468b51c37789f094b0cccbaf9a5b
Chester Enright <cenright@altamontco.com>
2019-07-06 15:48:22 -0500

    feat: creating frontend server for singular

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ RUN apt update && apt -y install aria2 \
 		python3 \
 		python3-pip
 
+
 ADD requirements.txt /
 RUN pip3 install -r requirements.txt
 
---
+++ server.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+Server for Frontend to singular
+'''
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+if __name__ == '__main__':
+        socketio.run(app)
