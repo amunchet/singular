70af96ed431e755e795d40e03e2bdb4d1e0aff45
Chester Enright <cenright@altamontco.com>
2019-10-06 01:28:08 -0500

    feat(ui): ui framework completed, no real backend yet

--- frontend/serve.py
+++ frontend/serve.py
@@ -3,3 +3,29 @@
 Backend service for the singular frontend
 Presently will probably just handle the settings.json file
 """
+import os
+import json
+
+from flask import Flask
+from flask  import request
+from flask_cors import CORS
+
+
+
+SETTINGS_FILE = "../../settings.json" # TODO: Change this properly
+if "SINGULAR_SETTINGS_FILE" in os.environ:
+    SETTINGS_FILE = os.environ["SETTINGS_FILE"]
+
+app = Flask("singular_frontend")
+CORS(app)
+
+@app.route("/get")
+def return_settings():
+    """Returns the output of settings.json"""
+    with open(SETTINGS_FILE) as f:
+        return json.dumps(json.load(f))
+
+if __name__ == "__main__":
+    app.debug = True
+    app.config["ENV"] = "development"
+    app.run()
--- frontend/src/components/Editor.vue
+++ frontend/src/components/Editor.vue
@@ -1,21 +1,80 @@
 <template>
-  <div class="hello">
-	      <v-jsoneditor v-model="json"
-                  >
-    </v-jsoneditor>
-		      <br />
-		      <b-button variant="primary" >Submit</b-button>
-      </div>
+	<b-container class='main'>
+		<b-row>
+		<b-col class='left'>
+			<b-card>
+			<h2>RSS Feeds</h2>
+<b-container>
+				<b-row v-for="items in json.rss_feed">
+					<b-input class='m-1 'style='width:80%; float:left;'
+						:value="items">
+					</b-input>
+					<b-button class='m-1' variant='danger'><b>X</b></b-button>
+				</b-row>
+				<b-row class='left'>
+					<b-button variant="primary">+ Add RSS Feed</b-button>
+				</b-row>
+				</b-container>
+
+			</b-card>
+
+			<b-card>
+				<h2>Shows</h2>
+<b-container>
+				<b-row v-for="items in json.shows" class='m-3'>
+					<b-col style='width:10%;'>
+						<img :src="items[1]" width=100%/><br />
+						<b-button class='mt-2' style='width:100%;' variant='primary'>Change</b-button>
+					</b-col>
+					<b-col style='width:90%'>
+					<b-input class='m-1 '
+						:value="items[0]">
+					</b-input>
+					<b-button class='m-1 float_left' variant='danger'><b>Dropped</b></b-button>
+					<b-button class='m-1 float_left' variant='success'><b>Completed</b></b-button>
+					</b-col>
+				</b-row>
+				<b-row class='left'>
+					<b-button variant="primary">+ Add RSS Feed</b-button>
+				</b-row>
+				</b-container>
+
+	
+			
+			</b-card><b-card>
+				<h2>Removed RSS Feeds</h2></b-card>
+			<b-card>
+				<h2>Dropped Shows</h2></b-card>
+		</b-col>
+		<b-col class='left'>
+			<b-button class='mb-3' variant="success" >Submit</b-button>
+		<v-jsoneditor v-model="json"
+			      class="editor"
+			      >
+		</v-jsoneditor>
+		</b-col>
+
+		</b-row>
+	</b-container>
 </template>
 
 <script>
 
 import VJsoneditor from 'vue-jsoneditor';
+import axios from 'axios'
 export default {
-  name: 'Editor',
-  components: {
-	  VJsoneditor
-  },
+	name: 'Editor',
+	components: {
+		VJsoneditor
+	},
+	mounted: function(){
+		axios.get("http://localhost:5000/get").then(data=>{
+			this.json = data.data
+		})
+
+	},
+	methods: {
+	},
 	data() {
 		return {
 			json: {
@@ -23,26 +82,37 @@ export default {
 			}
 		}
 	},
-  props: {
-    msg: String
-  }
+	props: {
+		msg: String
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.float_left{
+	float: left;
+}
 h3 {
-  margin: 40px 0 0;
+	margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
+}
+.left{ text-align: left !important; }
+.main{
+	margin: auto;
+	margin-top: 20px;
+}
+.editor{
+	height: 500px !important;
 }
 </style>
