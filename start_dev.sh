ed04959472db02023382da561ccef5f694fb26c7
Chester Enright <amunchet@gmail.com>
2020-01-08 01:13:07 -0600

    chore: wip - creating startup scripts

---
+++ start_dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Start singular development server
+
+# Step 1: Start up the frontend dev server with hot reload for vue
+
+# Step 2: Start up the backend with debug (no gunicorn)
+
---
+++ start_prod.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Starts up the production version of singular server
+
+# Step 1: Compile the vue to dist in src
+
+# Step 2: Start nginx with dist for frontend
+
+# Step 3: Start backend with gunicorn
