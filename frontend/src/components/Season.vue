ce049657a7e17bed64fb9da01875e414ba8c04c6
Chester Enright <amunchet@gmail.com>
2020-01-09 01:19:08 -0600

    feat(season): wip - season layout

--- frontend/src/components/Season.vue
+++ frontend/src/components/Season.vue
@@ -1,14 +1,28 @@
 <template>
+  <div>
+    Season
+    {{season}}
+    <b-select>
+    </b-select>
+
+    Year
+    <b-input v-model="year"/>
+  </div>
 </template>
 
 <script>
 export default{
   name: "Season",
+  props: ["season", "year"],
   data(){
     return {
     }
   },
   methods: {
+    calculateSeason: function(){
+      // Parse the current month and return approparite season best guess
+      
+    }
   },
   mounted: {
   }
