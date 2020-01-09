3c6abef210568b76748c32fccee8de47ee502cff
Chester Enright <amunchet@gmail.com>
2020-01-09 01:16:43 -0600

    feat: wip - added new season and year selector

---
+++ frontend/src/components/Season.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default{
+  name: "Season",
+  data(){
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted: {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
